5407feaecdb87d41eb099fbd641d695404ea8363
xinqiyang <xinqiyang@gmail.com>
2013-06-26 10:45:36 +0900

    add Lib Conf

--- Conf/config.php
+++ Conf/config.php
@@ -1,8 +1,14 @@
 <?php
 /**
- * FreeFlare CMS 
+ * FFTest
  * User: xinqiyang
  * Date: 6/26/13
  * Time: 10:40 AM
  * To change this template use File | Settings | File Templates.
  */
+
+return array(
+    'apiurl'=>'http://ko.freeflare.com', //apiurl
+
+
+);
